3b27bb0eb579c01f4d66b685b226a37bb69136bb
OceaneLao <oceane.lao1@gmail.com>
2024-01-08 16:59:07 +0100

    twig template

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+
+    #[Route(
+        "/article/{id}/",
+        name: "detail_article",
+        methods: ["get"],
+        requirements: ["id" => "\d+"]
+    )]
+
+    public function hello($id)
+    {
+        return $this->render("example.html.twig", [
+            "articles" => [
+                "titre1" => "Titre1",
+                "titre2" => "Titre2",
+                "titre3" => "Titre3"
+            ],
+            "visible" => true
+        ]);
+    }
+
+    // #[Route(
+    //     "/home",
+    //     name: "home",
+    //     methods: ["get"],
+    // )]
+
+    // public function home()
+    // {
+    //     return $this->render("home.html.twig");
+    // }
+}
