040d89e98c4d383512cdfeb4cdc3d7b6ffdbdce2
OceaneLao <oceane.lao1@gmail.com>
2024-01-12 16:14:11 +0100

    password hash, login and logout

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,145 +5,88 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-/* Supprimer 
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;*/
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/user')]
 class UserController extends AbstractController
 {
-    //Ajouter des méthodes pour récupérer les données dans les champs de saisie
-    #[Route('/user', name: 'app_user', methods: ['GET', 'POST'])]
-
-    // ADD (ajouter une entité)
-    public function index(Request $request, EntityManagerInterface $em): Response
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
     {
-        //Déclarer $form (fichier UserType.php)
-        $form = $this->createForm(UserType::class);
-
-        //createFormBuilder() permet de créer le formulaire
-        // $form = $this->createFormBuilder()
-        //->add('nom',Type::class)
-        // ->add('email', EmailType::class)
-        // ->add('password', PasswordType::class)
-        // ->add('submit', SubmitType::class)
-        // ->getForm()
-        // ; 
-
-        // handleRequest() est une méthode permettant de gérer le traitement de la saisie du formulaire.
-        $form->handleRequest($request);
-
-        // Conditions 
-        // Si le formulaire est envoyé (submit) et validé
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            // Récupérer les datas
-            $data = $form->getData();
-
-            // Récupérer l'entité 
-            $user = new User();
-
-            // Mapper les données des utilisateurs
-            $user->setEmail($data["email"]);
-            $user->setPassword($data["password"]);
-
-            // persist() permet de vérifier toutes les conditions pour l'insertion (exemple : id)
-            $em->persist($user);
-
-            // Donner l'autorisation d'insérer l'id 
-            $em->flush();
-            //dd($data, $user);
-        }
         return $this->render('user/index.html.twig', [
-            'form' => $form->createView(),
+            'users' => $userRepository->findAll(),
         ]);
     }
 
-    // LECTURE / READ
-    #[Route('/user/{id}', name: 'user_detail', requirements: ["id" => "\d+"])]
-    public function read($id, UserRepository $userRepository): Response
-    {
-        $user = $userRepository->find((int)$id);
-        //dd($user);
-        // Afficher la lecture
-        return $this->render("user/read.html.twig", [
-            "user" => $user,
-        ]);
-    }
-
-    // UPDATE
-    #[Route('/user/{id}/edit', 
-    name: 'user_edit', 
-    //Ajouter une méthode
-    methods: ['GET','POST'],
-    requirements: ["id" => "\d+"])]
-    
-    public function edit(
-        $id, 
-        //Ajouter Render Request
-        Request $request, 
-        UserRepository $userRepository,
-        // Besoin de la BDD
-        EntityManagerInterface $em
-        )
-    {
-        // Chercher le user 
-        $user = $userRepository->find((int)$id);
-        // dd($user);
-
-        // Créer le formulaire avec createForm() en ajoutant des datas (éviter des valeurs null)
-        $form = $this->createForm(UserType::class, $user);
-        // dd($form);
-
-        //Utilisation de la méthode handleRequest()
-        $form->handleRequest($request);
-        //Ajouter une condition
-        if ($form->isSubmitted() && $form->isValid() ){
-            //Déclarer une variable user
-            $user = $form->getData();
-            //Pas besoin de $em->persist() car pas de vérification 
-            $em->flush();
-            dd($form->getData());
-        }
-
-        //Retourner dans la vue
-        return $this->render("user/edit.html.twig", [
-            "form" => $form->createView(),
-        ]);
-    }
-
-    // DELETE
-    #[Route('/user/{id}/delete',
-    name: 'user_delete',
-    methods: ['GET'],
-    requirements: ["id" => "\d+"]
-    )]
-
-    public function delete(
-        $id, 
-        Request $request, 
-        UserRepository $userRepository,
-        EntityManagerInterface $em
+    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    public function new(
+        Request $request,
+        // Ajouter UserPasswordHasherInterface
+        UserPasswordHasherInterface $hasher,
+        EntityManagerInterface $entityManager
         ): Response
     {
-        $user = $userRepository->find((int)$id);
-        //Condition pour tester la méthode
-        if($request->query->get('method')=== 'DELETE'){
-            $em->remove($user);
-            $em->flush();
-            // dd($id);
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
-            //Redirection sur une autre route
-            return $this->redirectToRoute(('app_user'));
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Utilisation de la méthode setPassword de user
+            $user->setPassword($hasher->hashPassword($user, $user->getPassword()));
+            // dd($user);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
-        return $this->render("user/read.html.twig", [
-            "user" => $user,
+
+        return $this->render('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,78 +3,33 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $firstname = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $lastname = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
+    #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
     private ?string $password = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $img = null;
-
-    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
-    private ?\DateTimeInterface $birthdate = null;
-
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
-    private ?\DateTimeInterface $creation_date = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $description = null;
-
-    #[ORM\OneToMany(mappedBy: 'id_user', targetEntity: Appointment::class)]
-    private Collection $appointments;
-
-    public function __construct()
-    {
-        $this->appointments = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getFirstname(): ?string
-    {
-        return $this->firstname;
-    }
-
-    public function setFirstname(?string $firstname): static
-    {
-        $this->firstname = $firstname;
-
-        return $this;
-    }
-
-    public function getLastname(): ?string
-    {
-        return $this->lastname;
-    }
-
-    public function setLastname(?string $lastname): static
-    {
-        $this->lastname = $lastname;
-
-        return $this;
     }
 
     public function getEmail(): ?string
@@ -82,100 +37,63 @@ class User
         return $this->email;
     }
 
-    public function setEmail(?string $email): static
+    public function setEmail(string $email): static
     {
         $this->email = $email;
 
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
 
-    public function setPassword(?string $password): static
+    public function setPassword(string $password): static
     {
         $this->password = $password;
 
         return $this;
     }
 
-    public function getImg(): ?string
-    {
-        return $this->img;
-    }
-
-    public function setImg(?string $img): static
-    {
-        $this->img = $img;
-
-        return $this;
-    }
-
-    public function getBirthdate(): ?\DateTimeInterface
-    {
-        return $this->birthdate;
-    }
-
-    public function setBirthdate(?\DateTimeInterface $birthdate): static
-    {
-        $this->birthdate = $birthdate;
-
-        return $this;
-    }
-
-    public function getCreationDate(): ?\DateTimeInterface
-    {
-        return $this->creation_date;
-    }
-
-    public function setCreationDate(?\DateTimeInterface $creation_date): static
-    {
-        $this->creation_date = $creation_date;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(?string $description): static
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
     /**
-     * @return Collection<int, Appointment>
+     * @see UserInterface
      */
-    public function getAppointments(): Collection
+    public function eraseCredentials(): void
     {
-        return $this->appointments;
-    }
-
-    public function addAppointment(Appointment $appointment): static
-    {
-        if (!$this->appointments->contains($appointment)) {
-            $this->appointments->add($appointment);
-            $appointment->setIdUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeAppointment(Appointment $appointment): static
-    {
-        if ($this->appointments->removeElement($appointment)) {
-            // set the owning side to null (unless already changed)
-            if ($appointment->getIdUser() === $this) {
-                $appointment->setIdUser(null);
-            }
-        }
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
