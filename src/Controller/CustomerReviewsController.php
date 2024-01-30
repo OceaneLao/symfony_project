9beef4773c784f1dc4762ec00a437aabb2c8f640
OceaneLao <oceane.lao1@gmail.com>
2024-01-30 16:42:38 +0100

    customers reviews change size img

--- src/Controller/CustomerReviewsController.php
+++ src/Controller/CustomerReviewsController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 use App\Entity\CustomerReviews;
 use App\Form\CustomerReviewsType;
 use App\Repository\CustomerReviewsRepository;
+use Aws\Credentials\Credentials;
+use Aws\S3\Exception\S3Exception;
+use Aws\S3\S3Client;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/customer/reviews')]
 class CustomerReviewsController extends AbstractController
@@ -23,13 +27,51 @@ class CustomerReviewsController extends AbstractController
     }
 
     #[Route('/new', name: 'app_customer_reviews_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, 
+    EntityManagerInterface $entityManager,
+    SluggerInterface $slugger,
+    ): Response
     {
+        // phpinfo();
         $customerReview = new CustomerReviews();
         $form = $this->createForm(CustomerReviewsType::class, $customerReview);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->get('picture')->getData();
+            if ($image) {
+                $originalName = pathinfo(
+                    $image->getClientOriginalName(),
+                    PATHINFO_FILENAME
+                );
+
+                $nameSlugged = $slugger->slug($originalName);
+                $fileName = $nameSlugged . '-' . uniqid() . '.' . $image->guessExtension();
+                try {
+                    $sha256 = hash_file('sha256', $image->getRealPath());
+                    // Instantiate an Amazon S3 client.
+                    $credentials = new Credentials('AKIA47CRY3PFDBV5NM4F','pnI1eMF1AELEqxvm5VjX4ifnu8D2c0mI3Hbpy82G');
+                    $s3 = new S3Client([
+                        'version' => 'latest',
+                        'region'  => 'eu-north-1', //Services => Stockage => S3 => Région AWS
+                        'credentials' => $credentials
+                    ]);
+                    // Upload a publicly accessible file. The file size and type are determined by the SDK.
+                    $s3->putObject([
+                        'Bucket' => 'symfonyproject',
+                        'Key'    => $fileName, //Compte => Informations d'identification de sécurité => Clés d'accès
+                        'Body'   => $image,
+                        'ACL'    => 'public-read',
+                        'SourceFile' => $image->getRealPath(),
+                        'ContenFile' => $image->getMimeType(),
+                        'ContentSHA256' => $sha256
+                    ]);
+
+                } catch (S3Exception $error) {
+                    dd($error->getMessage());
+                }
+                $customerReview->setPicture($s3->getObjectUrl('symfonyproject', $fileName));
+            }
             $entityManager->persist($customerReview);
             $entityManager->flush();
 
--- src/Form/CustomerReviewsType.php
+++ src/Form/CustomerReviewsType.php
@@ -5,14 +5,20 @@ namespace App\Form;
 use App\Entity\CustomerReviews;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class CustomerReviewsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('picture')
+            ->add('picture', FileType::class,[
+                'label' => 'Photo',
+                'mapped' => false,
+                'required' => false
+            ])
             ->add('firstname')
             ->add('lastname')
             ->add('comment')
